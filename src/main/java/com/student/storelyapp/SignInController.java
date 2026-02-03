0e3047143bd5f45c648ea92eb05901234e961d49
Ciro Grez Aino <cirogreza7@gmail.com>
2026-02-03 22:05:27 +0100

    Update SignInController.java

--- src/main/java/com/student/storelyapp/SignInController.java
+++ src/main/java/com/student/storelyapp/SignInController.java
@@ -28,9 +28,9 @@ public class SignInController {
 
     private User addUser(String username, String masterPassword) {
 
-        final String DB_URL = "jdbc:postgresql://" + System.getenv("PGHOST") + ":" + System.getenv("PGPORT") + "/" + System.getenv("PGDATABASE");
-        final String USERNAME = System.getenv("PGUSER");
-        final String PASSWORD = System.getenv("PGPASSWORD");
+        final String DB_URL = "jdbc:mysql://" + System.getenv("MYSQLHOST") + ":" + System.getenv("MYSQLPORT") + "/" + System.getenv("MYSQLDATABASE");
+        final String USERNAME = System.getenv("MYSQLUSER");
+        final String PASSWORD = System.getenv("MYSQLPASSWORD");
 
         try (Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD)) {
 
